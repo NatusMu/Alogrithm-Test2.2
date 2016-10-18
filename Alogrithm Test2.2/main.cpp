2151387350244a66003f757ccc7cef6d8f318d3d
Artorius <937950943@qq.com>
2016-10-18 13:18:23 +0800

    Initial Commit

---
+++ Alogrithm Test2.2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Alogrithm Test2.2
+//
+//  Created by 黄沐 on 2016/10/18.
+//  Copyright © 2016年 Hm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
